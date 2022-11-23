d643328240d9bff54ac60d27d841764036c97d2b
AdamLaszab <adam.laszab@gmail.com>
2022-11-23 21:57:45 +0100

    Create priklady.py

---
+++ priklady.py
@@ -0,0 +1,38 @@
+##def funkcia():
+##    maxi=0
+##    akecislo=0
+##    x =[]
+##    while(True):
+##        print("Zadaj cislo")
+##        vstup=int(input())
+##        if(vstup==0):
+##            for item in x:
+##                if(maxi<x.count(item)):
+##                    maxi=x.count(item)
+##                    akecislo=item
+##            print(akecislo,maxi)
+##            break
+##        x.append(vstup)
+##
+##funkcia()
+
+##t = [1, 2, 3, 4]
+##def chop(t):
+##    t.pop(0)
+##    t.pop(-1)
+##
+##
+##chop(t)
+##print(t)
+a=[[1,2],[3,5]]
+b=[[5,2],[-3,4]]
+c=[[0]*len(a[0]) for _ in range(len(a))]
+def zrataj(a,b):
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            c[i][j]=a[i][j]+b[i][j]
+
+zrataj(a,b)
+print(c)
+
+    
