4886626b4197ceead3d35ef46a9cde9370989e1f
AdamLaszab <adam.laszab@gmail.com>
2022-11-27 13:08:38 +0100

    x

--- priklady.py
+++ priklady.py
@@ -16,7 +16,8 @@
 ##
 ##funkcia()
 
-##t = [1, 2, 3, 4]
+##t = [1, 2, 3, 4]
+
 ##def chop(t):
 ##    t.pop(0)
 ##    t.pop(-1)
@@ -24,15 +25,28 @@
 ##
 ##chop(t)
 ##print(t)
-a=[[1,2],[3,5]]
-b=[[5,2],[-3,4]]
-c=[[0]*len(a[0]) for _ in range(len(a))]
-def zrataj(a,b):
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            c[i][j]=a[i][j]+b[i][j]
+a=[[2,3,1],[4,3,5]]
+b=[[1,0,2],[3,3,-1],[-2,1,3]]
+# c=[[0]*len(a[0]) for _ in range(len(a))]
+# def zrataj(a,b):
+#     for i in range(len(a)):
+#         for j in range(len(a[0])):
+#             c[i][j]=a[i][j]+b[i][j]
 
-zrataj(a,b)
-print(c)
+# zrataj(a,b)
+# print(c)
 
-    
+def vynasob(a,b):
+    if(len(a[0])==len(b)):
+        c=[[0]*len(b[0]) for _ in range(len(a))]
+        for i in range(len(c)):
+            for j in range(len(c[0])):
+                spolu=0
+                for k in range(len(a[0])):
+                    spolu+=a[i][k]*b[k][j]
+                c[i][j]=spolu
+        return c
+    else:
+        print("zle")
+
+print(vynasob(a,b))
