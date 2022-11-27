3885b0665b4f233c46d67db1dd3e2358a069196e
AdamLaszab <adam.laszab@gmail.com>
2022-11-27 19:29:52 +0100

    x

--- priklady.py
+++ priklady.py
@@ -26,8 +26,8 @@ import random
 ##
 ##chop(t)
 ##print(t)
-a=[[2,3,1],[4,3,5]]
-b=[[1,0,2],[3,3,-1],[-2,1,3]]
+a=[1,2,10,2,2,5]
+# b=[[1,0,2],[3,3,-1],[-2,1,3]]
 # c=[[0]*len(a[0]) for _ in range(len(a))]
 # def zrataj(a,b):
 #     for i in range(len(a)):
@@ -52,4 +52,12 @@ b=[[1,0,2],[3,3,-1],[-2,1,3]]
 
 # print(vynasob(a,b))
        
-    
+
+def funkcia(t):
+    spolu=0
+    for item in t:
+        if(t.count(item)==1):
+            spolu+=1
+    return spolu
+
+print(funkcia(a))
