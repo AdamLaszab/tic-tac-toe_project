ccc5b1b432451b4a02f959b09a59c2dd57b6910c
AdamLaszab <adam.laszab@gmail.com>
2023-01-12 16:10:04 +0100

    x

--- priklady.py
+++ priklady.py
@@ -1,49 +1,24 @@
-import random
-##def funkcia():
-##    maxi=0
-##    akecislo=0
-##    x =[]
-##    while(True):
-##        print("Zadaj cislo")
-##        vstup=int(input())
-##        if(vstup==0):
-##            for item in x:
-##                if(maxi<x.count(item)):
-##                    maxi=x.count(item)
-##                    akecislo=item
-##            print(akecislo,maxi)
-##            break
-##        x.append(vstup)
-##
-##funkcia()
+t=[[1,2],[0,5],[6,7]]
+def test(x,y):
+    if(x[1]<y[0]):
+        return False
+    elif(x[0]>y[1]):
+        return False
+    else:
+        return True
+def funkcia(A):
+    z=[]
+    v=False
+    for j in range(len(A)):
+        for i in range(len(A)):
+            if(j!=i):
+                if(test(A[j],A[i])== True and A[i][0]<A[j][0] and A[j][1]<A[i][1]):
+                    v=True
+        if(v==False):
+            z.append(A[j])
+        v=False
 
-##t = [1, 2, 3, 4]
-
-##def chop(t):
-##    t.pop(0)
-##    t.pop(-1)
-##
-##
-##chop(t)
-##print(t)
-a=[1,2,10,2,2,5]
-# b=[[1,0,2],[3,3,-1],[-2,1,3]]
-# c=[[0]*len(a[0]) for _ in range(len(a))]
-# def zrataj(a,b):
-#     for i in range(len(a)):
-#         for j in range(len(a[0])):
-#             c[i][j]=a[i][j]+b[i][j]
+    return z
 
 
-
-# print(vynasob(a,b))
-       
-
-def funkcia(t):
-    spolu=0
-    for item in t:
-        if(t.count(item)==1):
-            spolu+=1
-    return spolu
-
-print(funkcia(a))
+print(funkcia(t))
