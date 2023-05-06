dd9d675663067d123c666505f025464fe1fe6dbb
AdamLaszab <adam.laszab@gmail.com>
2023-05-06 16:42:07 +0200

    Update priklady.py

--- priklady.py
+++ priklady.py
@@ -1,24 +1,3 @@
-t=[[1,2],[0,5],[6,7]]
-def test(x,y):
-    if(x[1]<y[0]):
-        return False
-    elif(x[0]>y[1]):
-        return False
-    else:
-        return True
-def funkcia(A):
-    z=[]
-    v=False
-    for j in range(len(A)):
-        for i in range(len(A)):
-            if(j!=i):
-                if(test(A[j],A[i])== True and A[i][0]<A[j][0] and A[j][1]<A[i][1]):
-                    v=True
-        if(v==False):
-            z.append(A[j])
-        v=False
+t="Adam"
 
-    return z
-
-
-print(funkcia(t))
+print(t.count("z"))
